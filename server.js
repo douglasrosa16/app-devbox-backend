9cefb573098583fc259937b44c7b6ddc001b509a
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2020-11-06 05:59:07 -0400

    add cadastro

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const app = express();
 
 app.use(cors());
 
-app.get('/users', (req, res) => {
-    return res.send({data});
+app.get('/users', (req, res) => {    
+    return res.json({data});
 });
+
+app.post('/cadastro', (req, res) => {
+    const dados = data;
+    console.log(res.data);
+    return res.json({dados});
+})
  
 module.exports = app;
