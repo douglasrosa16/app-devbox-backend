7d5b26d52965a0db1ddd26eda98c3b7552afe275
douglasrosa16 <42454362+douglasrosa16@users.noreply.github.com>
2020-11-20 16:38:57 -0400

    ajust server

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-const app = require('./server');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const Usuario = require('./models/User');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+    'mongodb+srv://devbox:devbox@cluster0.gpr2d.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useCreateIndex: true
+    }
+);
+
+app.get('/users', async(req, res) => {    
+    const users = await Usuario.find();
+    console.log(users);
+    return res.json({users});
+});
+
+app.post('/cadastro', async(req, res) => {
+    const {name, wpp: whatsapp, age: idade, mail: mail} = req.body;
+    
+    const user = await Usuario.create({
+        name,
+        email: mail,
+        age: idade,
+        whatsapp : whatsapp,        
+    })
+        
+    return res.json({ user });
+});
 
 app.listen(process.env.PORT || 8080); 
+
+ 
+
